32086b85e76b9eda0c0be22539fed2cdea4b1ed0
gitjms <46410240+gitjms@users.noreply.github.com>
2020-06-25 11:05:38 +0300

    initial commit2

---
+++ app.js
@@ -0,0 +1,34 @@
+const config = require('./utils/config')
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const peopleRouter = require('./controllers/people')
+const infoRouter = require('./controllers/info')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect( config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+app.use('/api/people', peopleRouter)
+app.use('/info', infoRouter)
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+app.use(middleware.custom_morgan)
+
+module.exports = app
---
+++ controllers/info.js
@@ -0,0 +1,31 @@
+const infoRouter = require('express').Router()
+const Person = require('../models/person')
+
+infoRouter.get('/', (request, response, next) => {
+  Person.find({})
+    .then(people => {
+      if (people) {
+        const result = {
+          num: people.length,
+          date: new Date(),
+        }
+        const results = '<title>Phonebook Info Page</title>'
+        + '<center><br /><strong style=\'font-size: 25px;\'>'
+        + 'Phonebook has info for ' + result.num + ' people'
+        + '</strong><br /><br />' + result.date + '<br /><br />'
+        + '<div className=\'col-auto\' align=\'center\'>'
+        + '<form action=\'/\' method=\'GET\'>'
+        + '<button type=\'submit\' onclick=\'/\'>Back</button>'
+        + '</form></div><br /><hr />'
+        + '<em>Phonebook app, Full Stack Web Development</em><br />'
+        + '<em>University of Helsinki 2020</em>'
+        + '</center>'
+        response.send(results)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+module.exports = infoRouter
